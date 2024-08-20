734f7bb64f9328fef70976a1b28b3639f3330bb8
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-08-20 10:09:46 +0700

    dashboard langsung redirect ke index master category

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -23,21 +23,23 @@ class DashboardController extends Controller
 
     public function index()
     {
-        $data['total_product'] = Product::count();
-        $data['total_user'] = User::count();
-        $data['total_pending'] = Order::where('status',0)->whereMonth('created_at', Date('m'))->whereYear('created_at',Date('Y'))->count();
-        $data['total_shipping'] = Order::where('status',2)->whereMonth('created_at', Date('m'))->whereYear('created_at',Date('Y'))->count();
-        $data['total_completed'] = Order::where('status',3)->whereMonth('created_at', Date('m'))->whereYear('created_at',Date('Y'))->count();
-        $data['total_order'] = $data['total_pending'] + $data['total_shipping'] + $data['total_completed'];
+        // $data['total_product'] = Product::count();
+        // $data['total_user'] = User::count();
+        // $data['total_pending'] = Order::where('status',0)->whereMonth('created_at', Date('m'))->whereYear('created_at',Date('Y'))->count();
+        // $data['total_shipping'] = Order::where('status',2)->whereMonth('created_at', Date('m'))->whereYear('created_at',Date('Y'))->count();
+        // $data['total_completed'] = Order::where('status',3)->whereMonth('created_at', Date('m'))->whereYear('created_at',Date('Y'))->count();
+        // $data['total_order'] = $data['total_pending'] + $data['total_shipping'] + $data['total_completed'];
 
-        $data['best_products'] = Product::with(['OrderDetails'])
-        ->withSum('OrderDetails','qty')
-        ->orderByDesc('order_details_sum_qty')
-        ->limit(10)
-        ->get();
-        $data['last_order'] = Order::orderBy('id','DESC')->limit(5)->get();
-        $data['chart'] = $this->chartOrder->build();
-        $data['chartPie'] = $this->chartOrderPie->build();
-        return view('backend.dashboard',compact('data'));
+        // $data['best_products'] = Product::with(['OrderDetails'])
+        // ->withSum('OrderDetails','qty')
+        // ->orderByDesc('order_details_sum_qty')
+        // ->limit(10)
+        // ->get();
+        // $data['last_order'] = Order::orderBy('id','DESC')->limit(5)->get();
+        // $data['chart'] = $this->chartOrder->build();
+        // $data['chartPie'] = $this->chartOrderPie->build();
+        // return view('backend.dashboard',compact('data'));
+
+        return redirect()->route('master.category.index');
     }
 }
