2d0dac2f1a5a0642246f60024761d784bdaec52e
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-08-20 10:11:04 +0700

    konfiguraasi ngrok

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // uncomment code jika ingin menggunakan ngrok
+        // comment code jika ingin menggunakan local
+        if(config('app.env') === 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
